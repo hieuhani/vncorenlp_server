9f698d14948d93fbb389bc1482ce3827bb425a2f
Hieu Tran <hieutran.fu@gmail.com>
2018-03-30 17:40:12 +0700

    Add Spark to support api query

--- src/main/kotlin/com/digitaltoken/vncorenlpserver/Hello.kt
+++ src/main/kotlin/com/digitaltoken/vncorenlpserver/Hello.kt
@@ -2,22 +2,33 @@ package com.digitaltoken.vncorenlpserver
 import com.google.gson.GsonBuilder
 import vn.pipeline.*
 import vn.pipeline.Annotation
+import spark.Spark.*
 
+
+val annotators = arrayOf("wseg", "pos", "ner", "parse")
+val pipeline = VnCoreNLP(annotators)
 
 fun main(args: Array<String>) {
-    val annotators = arrayOf("wseg", "pos", "ner", "parse")
-    val pipeline = VnCoreNLP(annotators)
+    get("/") {
+        request, response ->
 
-    val str = "Ông Nguyễn Khắc Chúc đang làm việc tại Đại học Quốc gia Hà Nội. Bà Lan, vợ ông Chúc, cũng làm việc tại đây."
-    val annotation = Annotation(str)
-    pipeline.annotate(annotation)
+        val input = request.queryParams("input")
+        if (input.isNullOrBlank()) {
+            response.status(201)
+            "Please specify query param: input"
+        } else {
+            val annotation = Annotation(input)
+            pipeline.annotate(annotation)
 
-    val output = GsonBuilder()
-            .registerTypeAdapter(Annotation::class.javaObjectType, AnnotationSerializer())
-            .registerTypeAdapter(Sentence::class.javaObjectType, SentenceSerializer())
-            .setPrettyPrinting()
-            .create()
+            val output = GsonBuilder()
+                    .registerTypeAdapter(Annotation::class.javaObjectType, AnnotationSerializer())
+                    .registerTypeAdapter(Sentence::class.javaObjectType, SentenceSerializer())
+                    .setPrettyPrinting()
+                    .create()
 
-    println(output.toJson(annotation))
+            response.type("application/json")
+            output.toJson(annotation)
+        }
+    }
 }
 
